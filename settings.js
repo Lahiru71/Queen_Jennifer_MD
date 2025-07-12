292036d892a84be540914cbfc9a84e384953a37b
Lahiru <lahirusathsara267@gmail.com>
2025-07-12 22:07:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,9 +14,10 @@ const { ownername, botname, footer, ig, region, myweb, packname, author, session
 
 //customize settings\\
 global.APIKeys = {'https://zenzapis.xyz': YOUR_API_KEY } //credit goes to sanzy yt, if api key expire, buy it and put it here?\\ https://zenzapis.xyz
-global.owner = ['94761539856'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
-global.premium = ['94761539856'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
-global.ownernomer = '94761539856' // Add Your number Eg: ['YOUR NUMBER']
+global.owner = ['94740859450'] // Add Your number Eg: ['94740859450
+' '94740859450
+global.premium = ['94740859450'] // Add Your number Eg: ['94740859450', '94740859450']
+global.ownernomer = '94740859450' // Add Your number Eg: ['94740859450']
 // we got your name in your whatsapp
 
 
